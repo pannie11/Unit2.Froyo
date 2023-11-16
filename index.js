40ebc579823b0b82593e7e0b246f71f610338d0b
Annie Pan <anniepan@Annies-MBP.fios-router.home>
2023-11-16 03:09:59 -0500

    edited the if statement and for loop to split the user input into multiple strings

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const userInput = prompt('Please enter the flavor(s) you want, separated by commas')
 
 if(userInput) {
-    console.log([userInput])
+    console.log(userInput.split(','))
+}
+
+let splitUserInput = userInput.split(',')
+for(i = 0; i < splitUserInput.length; i++) {
+    console.log(splitUserInput[i])
 }
