5130b0823cdb43e5203c09171038b60e204dff2a
Annie Pan <anniepan@Annies-MBP.fios-router.home>
2023-11-16 04:35:12 -0500

    put the if statement, for loop, and newly created object + forEach() method in a function called getFlavors, then invoked the function in the console

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const userInput = prompt('Please enter the flavor(s) you want, separated by commas')
 
-if(userInput) {
-    console.log(userInput.split(','))
+function getFlavors() {
+    if(userInput) {
+        console.log(userInput.split(','))
+    }
+    
+    const splitUserInput = userInput.split(',')
+    for(i = 0; i < splitUserInput.length; i++) {
+        const flavor = splitUserInput[i]
+        console.log(flavor)
+    }
+    
+    const flavors = {};
+    splitUserInput.forEach((input) => {
+        flavors[input] = (flavors[input] || 0) + 1
+    })
+    return flavors
 }
+console.log(getFlavors())
 
-let splitUserInput = userInput.split(',')
-for(i = 0; i < splitUserInput.length; i++) {
-    console.log(splitUserInput[i])
-}
+
+
