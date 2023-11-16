4f492aa5810735467740f9df76594c8ed9ded0ee
Annie Pan <anniepan@Annies-MBP.fios-router.home>
2023-11-16 04:35:34 -0500

    formatted the code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const userInput = prompt('Please enter the flavor(s) you want, separated by commas')
 
 function getFlavors() {
-    if(userInput) {
+    if (userInput) {
         console.log(userInput.split(','))
     }
-    
+
     const splitUserInput = userInput.split(',')
-    for(i = 0; i < splitUserInput.length; i++) {
+    for (i = 0; i < splitUserInput.length; i++) {
         const flavor = splitUserInput[i]
         console.log(flavor)
     }
-    
+
     const flavors = {};
     splitUserInput.forEach((input) => {
         flavors[input] = (flavors[input] || 0) + 1
